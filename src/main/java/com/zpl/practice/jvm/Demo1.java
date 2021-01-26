00849dd72d68af819c5272ee615bf2004c49f4d4
张沛霖 <zhangpeilinn@163.com>
2021-01-26 16:14:29 +0800

    save: 1. 增加 jvm 参数练习

---
+++ src/main/java/com/zpl/practice/jvm/Demo1.java
@@ -0,0 +1,28 @@
+package com.zpl.practice.jvm;
+
+/**
+ * @author 张沛霖
+ * @date 2021/1/15
+ */
+public class Demo1 {
+    //jvm 参数
+    //-XX:NewSize=5242880
+    // -XX:MaxNewSize=5242880
+    // -XX:InitialHeapSize=10485760
+    // -XX:MaxHeapSize=10485760
+    // -XX:SurvivorRatio=8
+    // -XX:PretenureSizeThreshold=10485760
+    // -XX:+UseParNewGC
+    // -XX:+UseConcMarkSweepGC
+    // -XX:+PrintGCDetails
+    // -XX:+PrintGCTimeStamps
+    // -Xloggc:gc.log
+    public static void main(String[] args) {
+        byte[] array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = null;
+
+        byte[] array2 = new byte[2 * 1024 * 1024];
+    }
+}
